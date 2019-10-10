49c05824eb2689b623ddcc8146c467d48bde13ae
Estudiante <Estudiante@ISIS2603-133.sis.virtual.uniandes.edu.co>
2019-10-09 22:14:15 -0500

    DtoGenero y DtoEnvio

---
+++ s3_vinilos-api/src/main/java/co/edu/uniandes/csw/vinilos/dtos/GeneroDto.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.vinilos.dtos;
+
+import co.edu.uniandes.csw.vinilos.entities.GeneroEntity;
+import java.io.Serializable;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class GeneroDto implements Serializable 
+{
+
+    private Long id;
+    
+    private String nombre;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public GeneroDto(GeneroEntity generoEntity) 
+    {
+        this.id = generoEntity.getId();
+        this.nombre = generoEntity.getNombre();
+    }
+ 
+    public GeneroEntity toEntity(){
+         GeneroEntity generoEntity = new GeneroEntity();
+         generoEntity.setId(this.getId());
+         generoEntity.setNombre(this.getNombre());
+         return generoEntity;
+    }
+     @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+    
+    
+    
+}
