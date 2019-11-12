42bbdefd54162e53322e5a9f39ddcf26fa5c5ac2
Stephania <s.otalorag@uniandes.edu.co>
2019-11-11 19:55:09 -0500

    Envio Get

--- s3_vinilos-api/src/main/java/co/edu/uniandes/csw/vinilos/resources/EnvioResource.java
+++ s3_vinilos-api/src/main/java/co/edu/uniandes/csw/vinilos/resources/EnvioResource.java
@@ -6,9 +6,18 @@
 package co.edu.uniandes.csw.vinilos.resources;
 
 import co.edu.uniandes.csw.vinilos.dtos.EnvioDTO;
+import co.edu.uniandes.csw.vinilos.ejb.EnvioLogic;
+import co.edu.uniandes.csw.vinilos.ejb.PedidoLogic;
+import co.edu.uniandes.csw.vinilos.entities.EnvioEntity;
+import co.edu.uniandes.csw.vinilos.exceptions.BusinessLogicException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,6 +32,9 @@ import javax.ws.rs.Produces;
 @RequestScoped
 public class EnvioResource {
     private static final Logger LOGGER = Logger.getLogger(EnvioResource.class.getName());
+    
+      @Inject
+    private EnvioLogic envioLogic;
      /**
      * Retorna un envio DTO
      *
@@ -31,8 +43,29 @@ public class EnvioResource {
      * @return El envio dto.
      */
       @POST
-      public EnvioDTO createEnviooDTO(EnvioDTO envio)
+      public EnvioDTO createEnviooDTO(EnvioDTO envio) throws BusinessLogicException
       {
-          return envio;
+           EnvioEntity envioEntity = envio.toEntity();
+        // Invoca la lógica para crear el pedido nueva
+        EnvioEntity nuevoEnvioEntity = envioLogic.createEnvio(envioEntity);
+        // Como debe retornar un DTO (json) se invoca el constructor del DTO con argumento el entity nuevo
+        EnvioDTO nuevoEnvioDTO = new EnvioDTO(nuevoEnvioEntity);
+        return nuevoEnvioDTO;
       }
+      
+      @GET
+    public List<EnvioDTO> getPedidos() {
+        //LOGGER.info("PedidoResource getPedidos: input: void");
+        List<EnvioDTO> listaEnvios = listEntityPedidos(envioLogic.getEnvio());
+        //LOGGER.log(Level.INFO, "PedidoResource getPedidos: output: {0}", listaEnvios);
+        return listaEnvios;
+    }
+    
+    private List<EnvioDTO> listEntityPedidos(List<EnvioEntity> entityList) {
+        List<EnvioDTO> list = new ArrayList<>();
+        for (EnvioEntity entity : entityList) {
+            list.add(new EnvioDTO(entity));
+        }
+        return list;
+    }
 }
